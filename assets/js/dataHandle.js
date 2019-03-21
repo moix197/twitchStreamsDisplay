2d5bf5c591d4138265bbbcded5ebca012984e2c2
Moises Montilla <cl_moises@hotmail.com>
2019-03-20 23:10:10 -0300

    Updated design patern to OLOO, removed jquery ajax and used fetch API instead, divided the app in modules so the code is easier to maintain.

---
+++ assets/js/dataHandle.js
@@ -0,0 +1,56 @@
+const dataHandle ={
+	//this object handle the data that comes from the "backend"
+	//and initialize our UI once we get the streams and game button data
+
+	apiCallUrl: 'https://api.twitch.tv/kraken/streams?client_id=kcrh2z2tj9qqs1asztf6n755zmwspns', // replace the {CLIENT_ID_HERE} with your twitch API client id
+	apiSelectionUrl: 'https://api.twitch.tv/kraken/games/top?client_id=kcrh2z2tj9qqs1asztf6n755zmwspns',  // replace the {CLIENT_ID_HERE} with your twitch API client id
+
+	init(){
+		this.getOnlineStreams('All');
+		this.getGames();
+	},
+
+	handleApiCall(apiURL,callback){
+		fetch(apiURL)
+			.then(res => res.json())
+			.then(response => {
+				callback(response);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	},
+
+	getGames(){
+		this.handleApiCall(this.apiSelectionUrl, (response) => {
+            // This call will initialize the game Selection Buttons (The ones on top)
+			this.initGamesSelectionBar(response);
+			//We don't handle error cases here because even if we don't get the games lists
+			//we can still show the streams for the "all" category which for this test is enough.
+			//if you're in a production enviroment you may want to
+		});
+	},
+
+	getOnlineStreams(){
+		// This call will render online streams for the selected game if no game is selected will display the most popular streams
+		this.loaderElement.style.display = "block";
+		this.handleApiCall(this.apiCallUrl, (response) => {
+			if(response && response.streams && response.streams.length > 0){
+				//Calling the function to render the streams after getting the api response
+				this.initStreams(response);
+			}else{
+				this.iniFallback();
+			}
+		});
+	}
+};
+
+export default dataHandle;
+//we can initialize it this way because this is 
+//bassically the full App 
+//twitchData.init();
+//if you want to initialize this webapp
+//in your page after any other event you can remove the IIFE
+//and give the function a name, then you can call it from
+//wherever you want in your app and you'll only be placing one name
+//in the global context.
---
+++ assets/js/uiComponents.js
@@ -0,0 +1,177 @@
+const uiComponents = {
+	outerElement: document.getElementById('outerStreams'),
+	loaderElement: document.getElementById('streamsLoader'),
+	btnElements: ['All'], //leave the All element, as it wont come from the API
+	activeTab: 'All',//this is the default activeTab, leave it as it's
+	streamsData: false,
+	buttonsData: false,
+
+	initGamesSelectionBar(data){
+        this.buttonsData = data;
+		this.handleSelectionBtns();
+		// if you want to do something else with the game buttons you can add it here
+	},
+
+	initStreams(data){
+		this.streamsData = data;
+		this.handleStreams();
+		this.setClickEvent('closeStream',this.closeStreamBtnEvent);
+		this.loaderElement.style.display = "none";
+		// if you want to do something else with the streams you can add it here
+	},
+
+	setClickEvent(itemID,callback){
+		document.getElementById(itemID).addEventListener("click", (e) => {
+			callback(itemID);
+		});
+	},
+
+	closeStreamBtnEvent(){
+		document.getElementById('outerGameSelection').scrollIntoView({ behavior: 'smooth' });
+		document.getElementById('liveVideoIframe').setAttribute('src', '');
+		document.getElementById('outerIframe').classList.remove('openedStream');
+		document.getElementById('closeStream').style.display = 'none';
+	},
+
+	streamClickEvents(streamId){
+		streamId = streamId.split('_').join('%20');
+		var	channel = 'http://player.twitch.tv/?channel='+streamId;
+		document.getElementById('liveVideoIframe').setAttribute('src',channel);
+		document.getElementById('outerIframe').classList.add('openedStream');
+		document.getElementById('closeStream').style.display = 'block';
+		document.getElementById('liveVideoIframe').scrollIntoView({ behavior: 'smooth' });
+	},
+	
+	gamesClickEvents(itemId){
+        if(this.activeTab == itemId){
+            return;
+        }
+        let gameName = itemId.replace(/_/g,'%20');
+
+        if(gameName == 'All'){
+            this.apiCallUrl = 'https://api.twitch.tv/kraken/streams?client_id=kcrh2z2tj9qqs1asztf6n755zmwspns';
+        }else{
+            this.apiCallUrl = 'https://api.twitch.tv/kraken/streams?game='+gameName+'&client_id=kcrh2z2tj9qqs1asztf6n755zmwspns';
+        }
+        
+        document.getElementById(this.activeTab).classList.remove("selectedGame");
+        document.getElementById(itemId).classList.add("selectedGame");
+        this.activeTab = itemId;
+        //the line below is for reseting the page before getting a new streams array
+        this.outerElement.innerHTML = '';
+        this.getOnlineStreams(itemId);
+	},
+
+	handleSelectionBtns(){
+		//here we map through the streams array to render each stream and
+        //set the proper click event.
+		this.buttonsData.top.map((element) =>{
+
+            let itemId = element.game.name.replace(/ /g,'_');
+			this.renderSingleSelectionBtn(element,itemId);
+			this.setClickEvent(itemId,(item) => {
+				this.gamesClickEvents(item);
+				//as the gamesClickEvents callback is using the "this" context
+				// we need to call it this way to assign the right context,
+				// you also have the option to bind the "this" context as I did
+				//in the line 88. ussually you want your code to be consistent
+				// but here I'm using the two methods just for explanation purposes.
+			});
+		});
+
+		this.setClickEvent('All',this.gamesClickEvents.bind(this));
+	},
+
+	renderSingleSelectionBtn(element,itemId){
+
+        let selectionBtn = document.createElement('img');
+		selectionBtn.setAttribute("src", element.game.box.medium);
+		selectionBtn.setAttribute("class", "selectionBtn");
+		selectionBtn.setAttribute("id", itemId);
+		document.getElementById('outerGameSelection').append(selectionBtn);
+		selectionBtn.setAttribute("alt", element.game.name+"_SelectionBtn");
+		this.btnElements.push(itemId);
+		
+	},
+
+	handleStreams(){
+		//here we map through the streams array to render each stream and
+		//set the proper click event.
+		this.streamsData.streams.map((element) => {
+			var streamId = element.channel.display_name.replace(/ /g,'_');
+			this.renderSingleStream(element, streamId);				
+			this.setClickEvent(streamId,this.streamClickEvents);
+		});
+	},
+
+	renderSingleStream(element, streamId){
+			/*Here we render the streams and append all the divs to the HTML document.*/
+			let SingleStream = document.createElement('div');
+			SingleStream.setAttribute("class", "singleStream");
+			SingleStream.setAttribute("id", streamId);
+	
+			let streamDetails = document.createElement('div');
+			streamDetails.setAttribute("class", "streamDetails");
+	
+			let outerImageStream = document.createElement('div');
+			outerImageStream.setAttribute("class", "outerImageStream");
+			let imageStream = document.createElement('img');
+			imageStream.src = element.preview.medium;
+	
+			let gameContainer = document.createElement('div');
+			gameContainer.setAttribute("class", "titleName gameName");
+			let gameLayoutContainer = document.createElement('p');
+			gameLayoutContainer.innerHTML = 'Game: ';
+			let gameNameContainer = document.createElement('p');
+			
+			let playerContainer = document.createElement('div');
+			playerContainer.setAttribute("class", "titleName");
+			let playerLayoutContainer = document.createElement('p');
+			playerLayoutContainer.innerHTML = 'User: ';
+			let playerNameContainer = document.createElement('p');
+			
+			let languageContainer = document.createElement('div');
+			languageContainer.setAttribute("class", "titleName");
+			let languageLayoutContainer = document.createElement('p');
+			languageLayoutContainer.innerHTML = 'Language: ';
+			let languageNameContainer = document.createElement('p');
+			
+			let viewersContainer = document.createElement('div');
+			viewersContainer.setAttribute("class", "titleName");
+			let viewersLayoutContainer = document.createElement('p');
+			viewersLayoutContainer.innerHTML = 'Viewers: ';
+			let viewersNameContainer = document.createElement('p');
+	
+			this.outerElement.append(SingleStream);
+				SingleStream.append(imageStream);
+				SingleStream.append(outerImageStream);
+				SingleStream.append(streamDetails);
+					outerImageStream.append(imageStream);
+					streamDetails.append(gameContainer);
+						gameContainer.append(gameLayoutContainer);
+						gameContainer.append(gameNameContainer);
+							gameNameContainer.append(element.game);
+					streamDetails.append(playerContainer);
+						playerContainer.append(playerLayoutContainer);
+						playerContainer.append(playerNameContainer);
+						playerNameContainer.append(element.channel.display_name);
+					streamDetails.append(viewersContainer);
+						viewersContainer.append(viewersLayoutContainer);
+						viewersContainer.append(viewersNameContainer);
+							viewersContainer.append(element.viewers);
+					streamDetails.append(languageContainer);
+						languageContainer.append(languageLayoutContainer);
+						languageContainer.append(languageNameContainer);
+							languageContainer.append(element.channel.language.toUpperCase());
+	},
+
+	initFallback(){
+		//Fallback in case the API response is empty
+		this.loaderElement.style.display = "none";
+		this.outerElement.innerHTML = "Sorry, we couldn't retrieve the streams data this time";
+		// Don't move this, so the users can't select another category before the previous one loads.	
+		this.activeTab = 'All';
+	}
+}
+
+export default uiComponents;
